60d690b302ffc9ae7e43c64818613bb4738a6c8f
Nogtkov <annogotkov@yandex.ru>
2018-07-20 16:41:13 +0300

    commit

--- app/js/sliderTeam.js
+++ app/js/sliderTeam.js
@@ -4,40 +4,39 @@ $(document).ready(function() {
       sliderLine = $('.our-staff__container-slider__slider-line'),
       ctShowSlides = 4;	
     var slide = sliderLine.children().eq(1);
-    var wrapper_circles = $('.our-staff__container-slider__wrapper-circles');
-    var circle = $('.our-staff__container-slider__wrapper-circles__circle');
+    var wrapper_circles = $('.our-staff__wrapper-circles');
+    var circle = $('.our-staff__wrapper-circles__circle');
 
+			ctShowSlides = 4;
 	setInterval(function(){
 		if(window.innerWidth > 1200) {
-			ctShowSlides = 4;
 		} else if (window.innerWidth <= 980 && window.innerWidth > 860) {
 			ctShowSlides = 3;
 		} else if (window.innerWidth <= 860 && window.innerWidth > 650) {
 			ctShowSlides = 2
 			if(wrapper_circles.children().size() <= 2) {
-				wrapper_circles.append('<span class="our-staff__container-slider__wrapper-circles__circle"></span>');
+				circle.eq(ctShowSlides-1).after('<span class="our-staff__wrapper-circles__circle"></span>');
+				alert(circle.size());
 			}
 		} else if (window.innerWidth <= 650) {
 			ctShowSlides = 1;
 		}
 		frameSlider.width((slide.width() * ctShowSlides) + slide.css('margin-left').slice(0,-2)*(ctShowSlides-1));
 	}, 1);
-
-	wrapper_circles.children().each(function(){
-		$(this).click(function() {
-	    	$(this).addClass('our-staff__container-slider__wrapper-circles__circle_active');
-	    	var offset = $(this).index()*(6 - ctShowSlides)*slide.width()*1.13;
-	    	alert(offset + " : " + sliderLine.width());
-	    	if(offset <= sliderLine.width()) {
+		wrapper_circles.children().click(function(){
+			alert($(this).index());
+	    	$(this).addClass('our-staff__wrapper-circles__circle_active');
+	    	var offset = $(this).index()*(6-ctShowSlides)*slide.width()*1.13;
+	    	alert(offset + " : " + sliderLine.width() + " : " + $(this).index());
+	  	 	if(offset <= sliderLine.width()) {
 	    		sliderLine.css('transform', 'translateX(-'+(offset)+'px)');
 	    	}
 
 	    	for(var i = 0; i < circle.size(); i++){
 	    		if($(this).index() != i){
-	    			circle.eq(i).attr('class','our-staff__container-slider__wrapper-circles__circle');
+	    			circle.eq(i).attr('class','our-staff__wrapper-circles__circle');
 	    		}
 	    	}
     	});
-    });
 
 })
