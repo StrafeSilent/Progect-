f64ccc5cd91fa67300e7c0b2bc813eb368268511
Nogtkov <annogotkov@yandex.ru>
2018-07-20 13:32:12 +0300

    не очень важный коммит

--- app/js/sliderTeam.js
+++ app/js/sliderTeam.js
@@ -4,31 +4,40 @@ $(document).ready(function() {
       sliderLine = $('.our-staff__container-slider__slider-line'),
       ctShowSlides = 4;	
     var slide = sliderLine.children().eq(1);
+    var wrapper_circles = $('.our-staff__container-slider__wrapper-circles');
     var circle = $('.our-staff__container-slider__wrapper-circles__circle');
 
-    circle.click(function(){
-    	$(this).toggleClass('our-staff__container-slider__wrapper-circles__circle_active');
-    	window.scrollTo(0, 200);
-    	for(var i = 0; i < circle.size(); i++){
-    		if($(this).index() != i){
-    			circle.eq(i).attr('class','our-staff__container-slider__wrapper-circles__circle');
-    		}
-    	}
+	setInterval(function(){
+		if(window.innerWidth > 1200) {
+			ctShowSlides = 4;
+		} else if (window.innerWidth <= 980 && window.innerWidth > 860) {
+			ctShowSlides = 3;
+		} else if (window.innerWidth <= 860 && window.innerWidth > 650) {
+			ctShowSlides = 2
+			if(wrapper_circles.children().size() <= 2) {
+				wrapper_circles.append('<span class="our-staff__container-slider__wrapper-circles__circle"></span>');
+			}
+		} else if (window.innerWidth <= 650) {
+			ctShowSlides = 1;
+		}
+		frameSlider.width((slide.width() * ctShowSlides) + slide.css('margin-left').slice(0,-2)*(ctShowSlides-1));
+	}, 1);
+
+	wrapper_circles.children().each(function(){
+		$(this).click(function() {
+	    	$(this).addClass('our-staff__container-slider__wrapper-circles__circle_active');
+	    	var offset = $(this).index()*(6 - ctShowSlides)*slide.width()*1.13;
+	    	alert(offset + " : " + sliderLine.width());
+	    	if(offset <= sliderLine.width()) {
+	    		sliderLine.css('transform', 'translateX(-'+(offset)+'px)');
+	    	}
+
+	    	for(var i = 0; i < circle.size(); i++){
+	    		if($(this).index() != i){
+	    			circle.eq(i).attr('class','our-staff__container-slider__wrapper-circles__circle');
+	    		}
+	    	}
+    	});
     });
-
-
-	setInterval(function() {
-	if(window.innerWidth > 1200) {
-		ctShowSlides = 4;
-	} else if (window.innerWidth <= 980 && window.innerWidth > 860) {
-		ctShowSlides = 3;
-	} else if (window.innerWidth <= 860 && window.innerWidth > 650) {
-		ctShowSlides = 2;
-	} else if (window.innerWidth <= 650) {
-		ctShowSlides = 1;
-	}
-	frameSlider.width((slide.width() * ctShowSlides) + slide.css('margin-left').slice(0,-2)*(ctShowSlides-1));
-	}, 10);
-
 
 })
